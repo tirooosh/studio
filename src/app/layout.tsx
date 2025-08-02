5464a2ba2586def4e6e94df7b54381b51016d23f
Tirosh Tayouri <tirosh.tayouri@gmail.com>
2025-08-02 23:10:50 +0000

    no manifest detected

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Toaster } from "@/components/ui/toaster"
 export const metadata: Metadata = {
   title: 'LinguaLecta',
   description: 'Your personal text-to-speech book reader.',
+  manifest: '/manifest.webmanifest',
 };
 
 export default function RootLayout({
@@ -19,7 +20,6 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet"></link>
         <link href="https://fonts.googleapis.com/css2?family=Literata:ital,opsz,wght@0,7..72,400;0,7..72,700;1,7..72,400&display=swap" rel="stylesheet" />
-        <link rel="manifest" href="/manifest.webmanifest" />
         <meta name="theme-color" content="#8FBC8F" />
         <script
           dangerouslySetInnerHTML={{
